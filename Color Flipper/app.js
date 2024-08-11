7a8902aa8524662db4f4ef448a3fd57050f8ab8a
srijanid <srijanidas06@gmail.com>
2024-08-11 17:09:01 +0530

    Color Flipper

---
+++ Color Flipper/app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color'); // Removed the dot
+
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(params) {
+    return Math.floor(Math.random() * colors.length); // Generate a random number
+}
